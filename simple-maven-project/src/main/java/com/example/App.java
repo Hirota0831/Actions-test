f8c56500a66516b0955f0691bc8f87f8e8b0684b
Hirota0831 <babolat0831@yahoo.co.jp>
2025-05-04 21:33:52 +0900

    Update App.java

--- simple-maven-project/src/main/java/com/example/App.java
+++ simple-maven-project/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven World!13");
+        System.out.println("Hello, Maven World!14");
     }
 }
